7387ebd325478f2107f2a6e0aeda39d09fba7e3f
Charmaine Helven G. Resma <77525866+chgresma@users.noreply.github.com>
2022-06-02 17:58:06 +0800

    RESMA - Login PHP  file

--- login.php
+++ login.php
@@ -1,8 +1,50 @@
-<html>
-    <body>
-        echo "Hello world";
-        echo "Hello world no2";
-        echo "Art Cute";
-        echo "June 2"
-    </body>
-</html>
+<?php 
+session_start(); 
+include "db_connection.php";
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+
+	$username = validate($_POST['username']);
+	$password = validate($_POST['password']);
+
+	if (empty($username)) {
+		header("Location: index.php?error=Username is required");
+	    exit();
+	}else if(empty($password)){
+        header("Location: index.php?error=Password is required");
+	    exit();
+	}else{
+		$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+
+		$result = mysqli_query($conn, $sql);
+
+		if (mysqli_num_rows($result) === 1) {
+			$row = mysqli_fetch_assoc($result);
+            if ($row['username'] === $username && $row['password'] === $password) {
+                    echo "Logged in!";
+            	$_SESSION['username'] = $row['username'];
+            	$_SESSION['name'] = $row['name'];
+            	$_SESSION['id'] = $row['id'];
+            	header("Location: home.php");
+		        exit();
+            }else{
+				header("Location: index.php?error=Incorect Username or Password");
+		        exit();
+			}
+		}else{
+			header("Location: index.php?error=Incorect Username or Password");
+	        exit();
+		}
+	}
+	
+}else{
+	header("Location: index.php");
+	exit();
+}
