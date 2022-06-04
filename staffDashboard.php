504f5e200364823580bede26f6a75ca742e97ee7
Gem <95133111+gemmygs@users.noreply.github.com>
2022-06-04 17:10:21 +0800

    updated staff dashboard's html
    need help with css, add space between Item and Item request kasi masyado magkadikit

--- staffDashboard.php
+++ staffDashboard.php
@@ -1,6 +1,114 @@
+<!DOCTYPE html>
 <html>
-    <body>
-        hi hello
+
+<head>
+    <title>Staff Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="StaffD-style.css">
+    <h1>STAFF DASHBOARD</h1>
+</head>
+
+<body>
+    <div>
+        <label style="float: center;">Welcome xx_aladin_xx!</label>
+
+
+        <div>
+            <label style="float:left;">
+            <div class="row">
+                <div class="column">
+                    <h2>ITEM</h2>
+                    <table>
+                        <!--INSERT PHP CODE HERE-->
+                        <tr>
+                            <tr>
+                                <th>Item Name</th>
+                                <th>Stock</th>
+                                <th>Price Per Stock</th>
+                                <th>Username</th>
+                            </tr>
+                            <tr>
+                                <td>Apple</td>
+                                <td>50</td>
+                                <td>10</td>
+                                <td>xx_aladin_xx</td>
+                            </tr>
+                            <tr>
+                                <td>Orange</td>
+                                <td>50</td>
+                                <td>10</td>
+                                <td>princessMae</td>
+                            </tr>
+
+                            </form>
+                    </table>
+                    <br>
+
+                    <form>
+
+                        <button type="button" style="margin-right: 5px;"> Update</button>
+                        <button type="reset" style="margin-right: 5px;"> Delete </button>
+                        <button type="button" style="margin-right: 5px;">Add</button>
+
+                        <select name="dropdown" id="dropdown_selected">
+                        <option value="">-SELECT-</option></select>
+
+
+                    </form>
+                </div>
+            <br> 
+            <br>
+
+                <div>
+   
+                    <div class="row">
+                        <div class="column">
+                            <h2>ITEM REQUEST</h2>
+                            <table>
+                                <!--INSERT PHP CODE HERE-->
+                                <tr>
+                                <tr>
+                                    <th>Request ID</th>
+                                    <th>Item Name</th>
+                                    <th>Quantity Request</th>
+                                    <th>Payment</th>
+                                    <th>Username</th>
+                                </tr>
+                                <tr>
+                                    <td>01</td>
+                                    <td>Orange</td>
+                                    <td>20</td>
+                                    <td>200</td>
+                                    <td>xx_aladin_xx</td>
+                                </tr>
+                                <tr>
+                                    <td>02</td>
+                                    <td>Apple</td>
+                                    <td>10</td>
+                                    <td>100</td>
+                                    <td>princessJ</td>
+                            </tr>
+                                    </form>
+                            </table>
+                            <br>
         
-    </body>
+                            <form>
+        
+                                <button type="button" style="margin-right: 10px;"> Request</button>
+                                <button type="reset" style="margin-right: 50px;"> Cancel </button>
+                                <select name="dropdown" id="dropdown_selected">
+                                <option value="">-SELECT-</option></select>
+    
+        
+                            </form>
+                        </div>
+
+             
+            </div>
+
+
+
+
+
+</body>
+
 </html>
