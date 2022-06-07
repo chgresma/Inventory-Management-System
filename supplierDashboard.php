a99eaabaebbffe85e288069ed2c64899ecb2ffa8
Kiyaruryn <kerr.labajo@cit.edu>
2022-06-07 16:16:33 +0800

    updated supplierDashboard

--- supplierDashboard.php
+++ supplierDashboard.php
@@ -14,7 +14,6 @@
         <div class="column">
         <h2>ITEM REQUEST</h2>
         <table>
-            <!--INSERT PHP CODE HERE-->
             <tr>
             <tr>
                 <th>Request ID</th>
@@ -33,7 +32,7 @@
                 $conn = mysqli_connect($sname, $username, $password, $db_name);
                 if($conn){
                     $query = "SELECT * FROM itemrequests";
-                    $requests = mysqli_query($conn,$query);//returned results
+                    $requests = mysqli_query($conn,$query);//returned results refer here
                     $check = mysqli_num_rows($requests);//result counter
                     if($check == 0){//if empty
                         //setonly 1 row of N/A if the table is empty
@@ -66,16 +65,33 @@
         </table></br>
         <form method = "post">
                 <select name ="dropdownItemRequest" id="dropdown_selectedItemRequest">
-                    <option value="---------------SELECT-----------------">-----------------SELECT-----------------</option>
+                    <option value="----SELECT----">----SELECT----</option>
+                    <?php
+                        if($conn){
+                            $list = "SELECT * FROM itemrequests";
+                            $sql = mysqli_query($conn,$list);
+                            while ($data = mysqli_fetch_assoc($sql)) {
+                                if(!empty($_POST['dropdownItemRequest']) && $_POST['dropdownItemRequest'] == $data['dropdownItemRequest']){
+                                    $selectedRequest = 'selected ="selected"';
+                                }
+                                else{
+                                    $selectedRequest = '';
+                                }
+                                ?>
+                                <option value = "<?php echo $data['itemName']; ?>" $selectedRequest><?php echo $data['itemName']; ?>
+                                </option>
+                                <?php
+                            }
+                        }
+                    ?>
                 </select>
-                <button type="submit" name = "deliver" style="margin-right: 135px;">Deliver</button>
+                <button type="submit" name = "deliver" style="margin-right: 135px;">Deliver Request</button>
         </form> 
         </div>
         
         <div class="column">
         <h2>DELIVERIES</h2>
         <table>
-            <!--INSERT PHP CODE HERE-->
             <tr>
                 <th>Delivery ID</th>
                 <th>Request ID</th>
@@ -122,11 +138,28 @@
             ?>
             </table> </br>
             <form method = "post">
-                <select name ="dropdownItemRequest" id="dropdown_selectedItemRequest">
-                    <option value="-----------------SELECT-----------------">-----------------SELECT-----------------</option>
-
+                <select name ="dropdownDeliveries" id="dropdown_selectedDeliveries">
+                    <option value="----SELECT----">----SELECT----</option>
+                    <?php
+                        if($conn){
+                            $list = "SELECT * FROM deliveryitem";
+                            $sql = mysqli_query($conn,$list);
+                            while ($data = mysqli_fetch_assoc($sql)) {
+                                if(!empty($_POST['dropdownDeliveries']) && $_POST['dropdownDeliveries'] == $data['dropdownDeliveries']){
+                                    $selectedDelivery = 'selected ="selected"';
+                                }
+                                else{
+                                    $selectedDelivery = '';
+                                }
+                                ?>
+                                <option value = "<?php echo $data['itemName']; ?>" $selectedRequest><?php echo $data['itemName']; ?>
+                                </option>
+                                <?php
+                            }
+                        }
+                    ?>
                 </select> 
-                <button type="submit" name="cancelDelivery">Cancel</button>
+                <button type="submit" name="cancelDelivery">Cancel Delivery</button>
             </form> 
             </div>
         </div>
@@ -139,12 +172,69 @@
             $postSelected_request = $_POST['dropdownItemRequest'];
             $must_select_req = "Must select a request to deliver";
             //to be continued in implementing the selected itemrequest to deliver
+            try{
+                if($postSelected_request != null){
+                    if(str_contains($postSelected_request, "----SELECT----")){
+                        throw new Exception($must_select_req);
+                    }
+                    else{
+                        $requestID = 0;
+                        $itemName = "";
+                        $quantityDelivered = 0;
+                        $payment = 0;;
+                        $staffUserName = "";
+                        $supplierUsername = $_SESSION["userName"];
+                        $getRequest = "SELECT * FROM itemrequests WHERE itemName = '".$postSelected_request."'";
+                        $gotRequest = mysqli_query($conn,$getRequest);
+                        while($row = mysqli_fetch_assoc($gotRequest)){
+                            $requestID = $row['requestID'];
+                            $itemName = $row['itemName'];
+                            $quantityDelivered = $row['quantityRequest'];
+                            $payment = $row['payment'];
+                            $staffUserName = $row['userName'];
+                        }
+                        $getPrice = "SELECT pricePerStock FROM items WHERE itemName = '".$postSelected_request."'";
+                        $gotPrice = mysqli_query($conn, $getPrice);
+                        $priceHolder = 0;
+                        while($prices = mysqli_fetch_assoc($gotPrice)){
+                            $priceHolder = $prices['pricePerStock'];
+                        }
+                        $expectedPay = $quantityDelivered * $priceHolder;
+                        $paymentChange = $payment - $expectedPay;
+                        
+                        $insertDelivery = "INSERT INTO deliveryitem (deliveryID, requestID, itemName, quantityDelivered, staff_userName, supplier_userName, paymentChange) 
+                                            VALUES(NULL, '".$requestID."', '".$itemName."', '".$quantityDelivered."', '".$staffUserName."', '".$supplierUsername."', '".$paymentChange."')";
+                        $deleteRequest = "DELETE FROM itemrequests WHERE itemName = '".$postSelected_request."'";
+                        mysqli_query($conn, $insertDelivery);
+                        echo "<script>alert('Successfully Delivered');  window.location.href = 'supplierDashboard.php';</script>";
+                        //mysqli_query($conn, $deleteRequest);
+                        
+                    }
+                }else{
+                    throw new Exception($must_select_req);
+                }
+            }catch(Exception $e){
+                echo "<script>alert('".$e->getMessage()."');</script>";
+            }
+
             
         }
         else if(isset($_POST['cancelDelivery'])){
             $postSelected_delivery = $_POST['dropdownDeliveries'];
             $must_select_del = "Must select a delivery to cancel.";
             //to be continued in implementing the selected delivery to cancel
+            try{
+                if(str_contains($postSelected_delivery, "----SELECT----")){
+                    throw new Exception($must_select_del);
+                }
+                $deleteDelivery = "DELETE FROM deliveryitem WHERE itemName = '".$postSelected_delivery."'";
+                if(mysqli_query($conn, $deleteDelivery)){
+                    echo "<script>alert('Successfully Deleted');  window.location.href = 'supplierDashboard.php'; </script>";
+                }
+            }catch(Exception $e){
+                echo "<script>alert('".$e->getMessage()."');</script>";
+            }
+            
         }
     }
 ?>
