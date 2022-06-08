86f3ece2d8eedad9d6a4c6dc60c8056063b8c4b8
cearr1112 <93184975+cearr1112@users.noreply.github.com>
2022-06-08 11:40:27 +0800

    Delete itemrequest.php

--- itemrequest.php
+++
@@ -1,7 +0,0 @@
-<?php
-    //attention art
-    //refer sa updateitem.php or additem.php for the design and layout
-    //extract the $_SESSION['username'] and $_SESSION['itemName'] to put in your sql
-    //you only have 2 inputs, Input Quantity to Request and Input Payment
-    //follow lang sa implementation sa additem for reference sa sql but itemrequests imo i implement
-?>
